8138b93016681c2f9ec6ac95b2f7eafc411c1947
abhishekbu008 <abhishekbu30@gmail.com>
2023-01-11 21:32:15 +0530

    Added Discount Grpc project

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,4 +1,5 @@
 using Discount.API.Extensions;
+using Discount.API.Repositories;
 
 namespace Discount.API
 {
@@ -14,6 +15,7 @@ namespace Discount.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddScoped<IDiscountRepository, IDiscountRepository>();
 
             var app = builder.Build();
 
